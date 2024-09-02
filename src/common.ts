f8e664c7b092c06323ce42d69793a360682c7bc3
Baris Aydek <baris.aydek@gmail.com>
2024-09-02 17:36:56 +0300

    removed proxy service from website

--- src/common.ts
+++ src/common.ts
@@ -1,6 +1,23 @@
 import { errorResponse } from "./response";
 import { Either } from "./utils";
 
+// TODO: actual verification needs to be like this:
+// secret won't be sent
+// secret is shared and kept in both sides
+// signature of secret and body will be sent in the headers
+/*
+
+payload_hmac = hmac.HMAC(
+    bytes(secret, "UTF-8"), body, digestmod=hashlib.sha256
+)
+base64_encoded_payload_hmac = base64.b64encode(
+    payload_hmac.digest()
+)
+
+return base64_encoded_payload_hmac.decode() == sig
+
+*/
+
 export const validateApiKey = (apiKeyName: string) => (reqHeaders: Headers) => {
   const apiKey = process.env[apiKeyName];
 
