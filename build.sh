f7491bb34e8c9525be0066528072844f0ddbfe58
Baris Aydek <baris.aydek@gmail.com>
2024-09-25 21:28:49 +0300

    build script for nip07-manager page

--- build.sh
+++ build.sh
@@ -8,3 +8,6 @@ mkdir pages
 # copy pages
 cp src/pages/nip65-manager.html pages/nip65-manager.html
 cp src/pages/nip65-manager.css pages/nip65-manager.css
+
+cp src/pages/nip07-manager.html pages/nip07-manager.html
+cp src/pages/nip07-manager.css pages/nip07-manager.css
