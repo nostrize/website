18e816d417b08d25e46a9d8e2cbc86536cd803c5
Baris Aydek <baris.aydek@gmail.com>
2024-10-02 02:24:46 +0300

    pages/nip07-metadata-manager.html

--- build.sh
+++ build.sh
@@ -9,5 +9,8 @@ mkdir pages
 cp src/pages/nip65-manager.html pages/nip65-manager.html
 cp src/pages/nip65-manager.css pages/nip65-manager.css
 
-cp src/pages/nip07-manager.html pages/nip07-manager.html
-cp src/pages/nip07-manager.css pages/nip07-manager.css
+cp src/pages/nip07-relays-manager.html pages/nip07-relays-manager.html
+cp src/pages/nip07-relays-manager.css pages/nip07-relays-manager.css
+
+cp src/pages/nip07-metadata-manager.html pages/nip07-metadata-manager.html
+cp src/pages/nip07-metadata-manager.css pages/nip07-metadata-manager.css
